44be3cb81db548d4f1e5dc252e3853a0f7609775
Flygenring <git@flygenring.eu>
2025-07-06 15:36:30 +0200

    Make the API fetch basic story information

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,11 +1,34 @@
-const fetchTopStories = async () =>
+const fetchTopStories = async (): Promise<ApiItemList> =>
 	await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+		.then(response => response.json())
+
+const fetchItem = async (itemId: number): Promise<ApiItem> =>
+	await fetch(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`)
 		.then(response => response.json())
 
 export default class {
 	static async getTopStories() {
-		const list = await fetchTopStories()
+		// Take only the specified number of stories
+		const list = (await fetchTopStories()).slice(0, 20)
 
-		return list.slice(0, 20)
+		// Inflate the list of IDs to hold the corresponding stories, and wait for all promises to
+		let storyList = await Promise.all(list.map(async (itemId: number) => await this.getStory(itemId)))
+
+		// Sort the list by score (subtracting 'a' from 'b' gives a descending sort)
+		storyList = storyList.sort((a, b) => b.score - a.score)
+
+		return storyList
+	}
+
+	static async getStory(storyId: number) {
+		const item = await fetchItem(storyId)
+
+		return {
+			id: item.id,
+			title: item.title,
+			username: item.by,
+			score: item.score,
+			url: item.url
+		}
 	}
 }
