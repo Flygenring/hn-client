431a7b31f5422d258e93ca5abe06b2071453d2a8
Flygenring <git@flygenring.eu>
2025-07-06 16:07:29 +0200

    Make and use an actual template and output the stories with actual data

--- src/App.tsx
+++ src/App.tsx
@@ -22,43 +22,17 @@ function App() {
 		<>
 			<h1>Latest top stories from Hacker News</h1>
 			{!stories.length && <div className="loader">Loading stories...</div>}
-			<div className="card">
-				{stories.join(", ")}
-			</div>
 			<div className="stories">
-				<div className="card">
+			{stories.map(story => (
+				<div className="card" key={story.id}>
 					<div className="meta">
-						<div className="score">731</div>
-						<div className="username">gasull</div>
+						<div className="score">{story.score}</div>
+						<div className="username">{story.username}</div>
 					</div>
-					<h2>Local-first software (2019)</h2>
-					<div className="url">https://www.inkandswitch.com/essay/local-first/</div>
+					<h2>{story.title}</h2>
+					<div className="url">{story.url}</div>
 				</div>
-				<div className="card">
-					<div className="meta">
-						<div className="score">478</div>
-						<div className="username">cxr</div>
-					</div>
-					<h2>Hidden interface controls that affect usability</h2>
-					<div className="url">https://interactions.acm.org/archive/view/july-august-2025/stop-hiding-my-controls-hidden-interface-controls-are-affecting-usability</div>
-				</div>
-				<div className="card">
-					<div className="meta">
-						<div className="score">228</div>
-						<div className="username">agcat</div>
-					</div>
-					<h2>How to Network as an Introvert</h2>
-					<div className="url">https://aginfer.bearblog.dev/how-to-network-as-an-introvert/</div>
-				</div>
-				<div className="card">
-					<div className="meta">
-						<div className="score">16</div>
-						<div className="username">mattigames</div>
-					</div>
-					<h2>Ask HN: If AGI were invented tomorrow which countries would fare better?</h2>
-					<div className="url"></div>
-					<div className="text">I know it's unlikely to be available tomorrow or sometime soon but as an hypothetical question.<p>Also, which countries would fare worse? And why?</p></div>
-				</div>
+			))}
 			</div>
 		</>
 	)
