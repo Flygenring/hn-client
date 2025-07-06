173f85a5f5d9cafa2a0fd85dc6443b2682dde246
Flygenring <git@flygenring.eu>
2025-07-06 19:47:13 +0200

    Clean components a little and add back the indicator for whether the story has any body text

--- src/component/StoryDetails.tsx
+++ src/component/StoryDetails.tsx
@@ -11,7 +11,6 @@ export default function StoryDetails(chosen) {
 					</div>
 				</div>
 				<h2>{chosen.story.title}</h2>
-				<div className="url">{chosen.story.url ? chosen.story.url : '—no URL included in this story—'}</div>
 				<div className="link">
 					{chosen.story.url && <div>Read more at: <a href={chosen.story.url}>{chosen.story.url}</a></div>}
 					{!chosen.story.url && <div>This story has no associated URL.</div>}
--- src/component/StoryList.tsx
+++ src/component/StoryList.tsx
@@ -2,14 +2,15 @@ export default function StoryList(list) {
 	return (
 		<>
 		{list.stories.map((story, index) => (
-			<button onClick={() => list.expandStory(list.stories[index])} className="card" key={story.id}>
+			<div onClick={() => list.expandStory(list.stories[index])} className="card" key={story.id}>
 				<div className="meta">
 					<div className="score">{story.score}</div>
 					<div className="username">{story.author.username}</div>
+					<div className="has-body">{story.text ? " 📖" : " 📕"}</div>
 				</div>
 				<h2>{story.title}</h2>
 				<div className="url">{story.url ? story.url : '—no URL included in this story—'}</div>
-			</button>
+			</div>
 		))}
 		</>
 	)
